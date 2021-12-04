df89abc67db157f9b092b81147b94b08d300f01d
biver25 <ya.ilya.burtsev@yandex.ru>
2021-12-04 22:27:21 +0300

    refactoring: added return in auth function

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,6 +8,7 @@ const auth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
     next(new NeedAuthorizationError());
+    return;
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
